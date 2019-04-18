add40b9b5f5833b653f0365880d5d80e9af7c761
Jonathan Wong <chi.jonathanwong@gmail.com>
2019-04-18 00:53:48 -0700

    Add components

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,15 @@ import logo from '../assets/images/thetatau.png'
 class Header extends React.Component {
     render() {
         return (
-            <section id="header">
+            <section id="header" class ="home-background">
                 <div className="inner">
                     <img id="logo-vector" src={logo} alt="Logo"></img>
                     <h1> <strong>Theta Tau</strong> 
                     </h1>
                     <p><b>Co-ed Professional Engineering Fraternity | SJSU Chapter</b></p>
+                    <div id="extend-height"></div>
                     <ul className="actions">
-                        <li><a href="#one" className="button scrolly">Rush</a></li>
+                        <li><a href="rush" className="button scrolly">Rush</a></li>
                     </ul>
                 </div>
             </section>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,12 +4,16 @@ class NavBar extends React.Component {
     render() {
         return (
             <section id="fixed-nav-bar">
-                <nav>
-                    <a href="#">Theta Tau | SJSU</a>
-                    <a href="#home">Home</a>
-                    <a href="#home">About</a>
-                    <a href="brothers">Brothers</a>
-                    <a href="rush">Rush</a>
+                <nav id="inner-nav">
+                    <div class="nav-left">
+                        <a href="/">Theta Tau | SJSU</a>
+                    </div>
+                    <div class="nav-right">
+                        <a href="/">Home</a>
+                        <a href="about">About</a>
+                        <a href="brothers">Brothers</a>
+                        <a href="rush">Rush</a>
+                    </div>
                 </nav>
             </section>
         )
