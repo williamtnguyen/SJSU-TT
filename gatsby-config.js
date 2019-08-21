ba444736eb01d3a94cc0c35b55169622ff408947
John Tran <jptran318@gmail.com>
2019-08-20 19:02:16 -0700

    Formatted files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,23 @@
 module.exports = {
   siteMetadata: {
     title: "SJSU Theta Tau Website",
-    description: "A Gatsby.js Starter based on Photon by HTML5 UP"
+    description: "A Gatsby.js Starter based on Photon by HTML5 UP",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/assets/images/website-icon.png", // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    "gatsby-plugin-sass",
+    "gatsby-plugin-offline",
   ],
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,41 +1,41 @@
-import React from "react";
+import React from "react"
 
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./Header"
+import Footer from "./Footer"
 import NavBar from "./NavBar"
 
 class Template extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        loading: 'is-loading'
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: "is-loading",
     }
+  }
 
-    componentDidMount () {
-      this.timeoutId = setTimeout(() => {
-          this.setState({loading: ''});
-      }, 100);
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => {
+      this.setState({ loading: "" })
+    }, 100)
+  }
+
+  componentWillUnmount() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId)
     }
+  }
 
-    componentWillUnmount () {
-      if (this.timeoutId) {
-          clearTimeout(this.timeoutId);
-      }
-    }
+  render() {
+    const { children } = this.props
 
-    render() {
-        const { children } = this.props;
-
-        return (
-            <div className={`body ${this.state.loading}`}>
-                <NavBar />
-                <Header />
-                {children}
-                <Footer />
-            </div>
-        );
-    }
+    return (
+      <div className={`body ${this.state.loading}`}>
+        <NavBar />
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    )
+  }
 }
 
-export default Template;
+export default Template
