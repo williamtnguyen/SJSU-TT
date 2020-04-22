0b6a513a9f911c09e0c062df750b0b34ac0f7244
Mohammad Sleiman <modysleiman@gmail.com>
2020-04-21 20:39:26 -0700

    Added Login
    Added User Authentication through Google Firebase
    Login persist through page refreshes
    User email is displayed on Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,59 @@
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+//import Navbar from 'react-bootstrap/Navbar';
+//import Nav from 'react-bootstrap/Nav';
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import firebase, { auth, provider } from '../services/firebase.js';
 
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: null
+    }
+    this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
+  }
+  login() {
+    auth.signInWithPopup(provider).then((result) => {
+      this.setState({
+          user: result.user
+      })
+    })
+  }
+  logout() {
+    auth.signOut().then((result) => {
+      this.setState({
+        user: null
+      })
+    })
+  }
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({user})
+      }
+    })
+  }
+
   render() {
+    let authButton = this.state.user ?
+      <GatsbyLink onClick={this.logout}>Log Out</GatsbyLink> :
+      <GatsbyLink onClick={this.login}>Log In</GatsbyLink>
+    let userInfo = this.state.user ?
+      <GatsbyLink> {this.state.user.email} </GatsbyLink> :
+      null
+    
     return (
+      
       <Navbar
         expand="lg"
+        variant = "dark"
         style={{ padding: '20px 10px', backgroundColor: '#883a3a' }}
       >
         <GatsbyLink to="/">
           <span className="navbar-brand mb-0 h1" style={{ color: 'white' }}>
-            Theta Tau | SJSU
+            Theta Tau V2.0  | SJSU
           </span>
         </GatsbyLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -22,16 +63,27 @@ class NavBar extends React.Component {
               <GatsbyLink to="/">Home</GatsbyLink>
             </Nav.Item>
             <Nav.Item>
-              <GatsbyLink to="/about">About</GatsbyLink>
+              <GatsbyLink to="/about">Who We Are</GatsbyLink>
             </Nav.Item>
+
+            <NavDropdown title="Rush" className="navdropdown">
+              <Nav.Item>
+                <GatsbyLink to="/rush">Rushing</GatsbyLink>
+              </Nav.Item>
+              <Nav.Item>
+              <a href="https://forms.gle/vpZQPmBSQcVA2MeJ8">Apply</a>
+              </Nav.Item>
+            </NavDropdown>
+
             <Nav.Item>
               <GatsbyLink to="/brothers">Brothers</GatsbyLink>
             </Nav.Item>
+            
             <Nav.Item>
-              <GatsbyLink to="/rush">Rush</GatsbyLink>
+              {authButton}  
             </Nav.Item>
             <Nav.Item>
-              <a href="https://forms.gle/vpZQPmBSQcVA2MeJ8">Apply</a>
+              {userInfo}
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
