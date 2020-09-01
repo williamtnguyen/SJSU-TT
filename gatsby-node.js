f95f33651c539da1ff65a3d6cc53f87fa62cfbde
William Nguyen <williamnguyen214@gmail.com>
2020-09-01 13:32:03 -0700

    feat: Add client-side dynamic routing to frontend
    Addresses #23

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,3 +26,21 @@ exports.onCreateWebpackConfig = ({ actions, stage, loaders }) => {
   }
   actions.setWebpackConfig(config);
 };
+
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+
+  // Only update the `/portal` page.
+  if (page.path.match(/^\/portal/)) {
+    // page.matchPath is a special key that's used for matching pages
+    // with corresponding routes only on the client.
+
+    // eslint-disable-next-line no-param-reassign
+    page.matchPath = '/portal/*';
+
+    // Update the page.
+    createPage(page);
+  }
+};
