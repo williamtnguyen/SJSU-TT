126bc1b0e79adca8c0396748f2b6acaa525c4672
dtuckersuh <suh977@gmail.com>
2020-10-25 22:08:38 -0700

    fix(server): Create edit validation and fix put endpoint
    Addresses #40

---
+++ routes/api/util/form-validation/edit.js
@@ -0,0 +1,27 @@
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+/**
+ * Takes in edit form input and checks for validity
+ * @param requestBody the data object holding all form inputs as fields
+ */
+function validateEditInput(requestBody) {
+  const errors = {};
+  // Only validate email and gradYear because they have more strict input
+  if (!Validator.isEmail(requestBody.email)) {
+    errors.email = 'Email field must be valid';
+  }
+  if (!Validator.isNumeric(requestBody.graduatingYear)) {
+    errors.graduatingYear = 'Graduate year must be a number';
+  } else if (
+    !Validator.isLength(requestBody.graduatingYear, { min: 4, max: 4 })
+  ) {
+    errors.graduatingYear = 'Graduate year must be valid year';
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+}
+
+module.exports = validateEditInput;
