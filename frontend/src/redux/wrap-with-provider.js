9cee9d4a28fb7f95b6cb5ee16ab6c08004785871
William Nguyen <williamnguyen214@gmail.com>
2020-12-13 00:16:51 -0800

    Wrap redux provider in gatsby-ssr and gatsby-browser

---
+++ frontend/src/redux/wrap-with-provider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+const wrapWithProvider = ({ element }) => {
+	return <Provider store={store}>{element}</Provider>
+}
+
+export default wrapWithProvider;
