31ef487621fda7373b32a9f7f9ca19659dba4e77
williamtnguyen <42355738+williamtnguyen@users.noreply.github.com>
2021-03-06 17:56:06 -0800

    Remove legacy frontend (#98)

--- frontend-legacy/src/util/curr-pledge-class.js
+++
@@ -1,6 +0,0 @@
-const { PledgeClassEnum } = require('./enums/brother-enums');
-
-/* No pledge class: set to null, otherwise use the enum */
-const CURR_PLEDGE_CLASS = null;
-
-module.exports = CURR_PLEDGE_CLASS;
