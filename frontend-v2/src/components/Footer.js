2b8e815b1e43d47408c84f35ad01d39e78d1e0af
williamtnguyen <williamnguyen214@gmail.com>
2022-01-09 13:56:29 -0800

    Remove font-awesome and opt for antd-icons

--- frontend-v2/src/components/Footer.js
+++ frontend-v2/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { InstagramOutlined, MailOutlined } from '@ant-design/icons';
 import thetaTauCrest from '../images/theta-tau-crest-white.png';
 import footerStyles from '../styles/components/footer.module.scss';
 
@@ -19,6 +20,14 @@ const Footer = () => {
         "Whatsoever thy hand findeth to do, do it with thy might..."
         —Ecclesiastes 9:10
       </p>
+      <div className={footerStyles.footer__icon__container}>
+        <a href="https://www.instagram.com/sjsuthetatau/" className={footerStyles.footer__icon}>
+          <InstagramOutlined />
+        </a>
+        <a href="mailto:thetatau.sjsu@gmail.com" className={footerStyles.footer__icon}>
+          <MailOutlined />
+        </a>
+      </div>
     </div>
   );
 };
